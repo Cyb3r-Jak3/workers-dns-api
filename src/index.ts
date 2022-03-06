5217f2c75dd220b1d6ff12f6ff0e9996e1e4ccfb
Jacob White <jake@jwhite.network>
2022-03-06 15:30:47 -0500

    Use api as base

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,17 @@ import { DNSCryptInfo, GetUsedDNSServer } from './dns-servers'
 
 const router = Router()
 
-router.get('/', () => {
+router.get('/api/', () => {
   return new Response(
     'Homepage for DNS API. Nothing here right now but there are other endpoints. Checkout the repo: https://github.com/Cyb3r-Jak3/workers-dns-api',
   )
 })
 
-router.get('/nameservers/used', GetUsedDNSServer)
+router.get('/api/nameservers/used', GetUsedDNSServer)
 
-router.get('/nameservers/all', DNSCryptInfo)
+router.get('/api/nameservers/all', DNSCryptInfo)
 
-router.get('/query', MiddlewareJSONCheck, queryEndpoint)
+router.get('/api/query', MiddlewareJSONCheck, queryEndpoint)
 
 router.all('*', () => new Response('404, not found!', { status: 404 }))
 
