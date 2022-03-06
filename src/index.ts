ce593ec6340972b956b93c860ff8f95909c6809d
Jacob White <jake@jwhite.network>
2022-03-06 15:33:11 -0500

    Add base API to router

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,19 @@ import { Router } from 'itty-router'
 import { MiddlewareJSONCheck } from './utils'
 import { DNSCryptInfo, GetUsedDNSServer } from './dns-servers'
 
-const router = Router()
+const router = Router({base: "/api"})
 
-router.get('/api/', () => {
+router.get('/', () => {
   return new Response(
     'Homepage for DNS API. Nothing here right now but there are other endpoints. Checkout the repo: https://github.com/Cyb3r-Jak3/workers-dns-api',
   )
 })
 
-router.get('/api/nameservers/used', GetUsedDNSServer)
+router.get('/nameservers/used', GetUsedDNSServer)
 
-router.get('/api/nameservers/all', DNSCryptInfo)
+router.get('/nameservers/all', DNSCryptInfo)
 
-router.get('/api/query', MiddlewareJSONCheck, queryEndpoint)
+router.get('/query', MiddlewareJSONCheck, queryEndpoint)
 
 router.all('*', () => new Response('404, not found!', { status: 404 }))
 
